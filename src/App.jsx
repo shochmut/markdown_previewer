1a11093942fe3dffc8c49c743714ca055f726554
shochmut <50213094+shochmut@users.noreply.github.com>
2023-02-11 13:18:58 -0700

    1 binaries uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,21 @@
 import { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { marked } from 'marked';
 
-let DEFAULTTEXT = `# Welcome to my React Markdown Previewer!
-
-## This is a sub-heading...
-### And here's some other cool stuff:
-
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
-
-There's also [links](https://www.freecodecamp.org), and
-> Block Quotes!
-
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | -------------
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
-
+let DEFAULTTEXT = `# heading element \n 
+## sub heading element \n 
+[link element](https://www.freecodecamp.org) \n 
+Inline Code: \`<div></div>\` \n 
+A Code Block: \`\`\` // this is multi-line code: \`\`\` text="hello world" \`\`\` \n 
 - And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
-
-
-1. And there are numbered lists too.
-1. Use just 1s if you want!
-1. And last but not least, let's not forget embedded images:
-
+- Some are bulleted.
+   - With different indentation levels.
+      - That look like this. \n
+> Block Quotes! \n 
 ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
-`
+` 
 
-DEFAULTTEXT += ```
-// this is multi-line code:
-text = 'Hello World';
-}
-```;
-
-DEFAULTTEXT += 'Heres some code, `<div></div>`, between 2 backticks.';
 
 function App() {
   const [text, setText] = useState(DEFAULTTEXT);
@@ -65,6 +39,7 @@ function Editor({ childToParent, text }) {
       Markdown Editor:
       <textarea
         id="editor"
+        defaultValue={DEFAULTTEXT}
         text={text}
         onChange={() => childToParent(event.target.value)}
       />
@@ -73,11 +48,14 @@ function Editor({ childToParent, text }) {
 }
 
 function Preview({ parentToChild }) {
-  const html = marked.parse(parentToChild);
+  let marktext = parentToChild
 
   return (
-    <div id="preview">
-      <p>{html}</p>
+    <div>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{ __html: marked(marktext) }}
+      ></div>
     </div>
   );
 }
